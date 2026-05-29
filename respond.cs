fbf4d92ff0b351e9aa1efaa66a1182bd9353d5cf
Student <Student@LabVM2049939>
2026-05-29 23:28:15 +0200

    Add project files.

---
+++ respond.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace list_view_chats
+{
+    internal class respond
+    {
+        private ArrayList reply;
+        private ArrayList ignore;
+
+        public respond(ArrayList reply, ArrayList ignore)
+        {
+            this.reply = reply;
+            this.ignore = ignore;
+        }
+    }
+}
